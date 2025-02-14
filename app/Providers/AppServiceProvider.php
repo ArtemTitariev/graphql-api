a5a96a1ca10bff9307c12256417922287dd82c6f
Artem Titariev <artem.titariev@gmail.com>
2025-02-14 20:32:40 +0200

    Add policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+use App\Policies\CommentPolicy;
+use App\Policies\PostPolicy;
+use App\Policies\TagPolicy;
+use App\Policies\UserPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Comment::class, CommentPolicy::class);
+        Gate::policy(Post::class, PostPolicy::class);
+        Gate::policy(Tag::class, TagPolicy::class);
+        Gate::policy(User::class, UserPolicy::class);
     }
 }
